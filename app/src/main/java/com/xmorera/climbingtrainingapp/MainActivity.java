7e1bd693d85bd7fe870cb83ccac1d7b6b2c1c4fc
xmorera-joviat <xmorera@joviat.cat>
2024-12-21 13:27:53 +0100

    veure data actual i seleccionar data

--- app/src/main/java/com/xmorera/climbingtrainingapp/MainActivity.java
+++ app/src/main/java/com/xmorera/climbingtrainingapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.xmorera.climbingtrainingapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView dateTextView;
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        dateTextView = findViewById(R.id.dateTextView);
+        updateDateTextView();
+
+        dateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+    }
+
+    private void updateDateTextView() {
+        String currentDate = dateFormat.format(calendar.getTime());
+        dateTextView.setText(currentDate);
+    }
+
+    private void showDatePicker(){
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, month);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        updateDateTextView(); // Update TextView with new date
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
     }
 }
